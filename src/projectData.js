040867c4c96f083f7a480fe9782339586ba721f2
Ahmed Osman <ahmedosman36610@gmail.com>
2022-10-25 13:14:17 -0700

    Added habits dashboard project

--- src/projectData.js
+++ src/projectData.js
@@ -1,6 +1,15 @@
 export const projectData = [
   {
     id: 1,
+    title: 'Habits Dashboard',
+    image: 'images/habitsdashboard.png',
+    description: `Implemented the front end for a dashboard that allows users to curate a list of quantifiable habits and then track their progress using a calendar heatmap and a line graph.`,
+    techStack: ['JSX', 'CSS', 'React', 'Redux', 'ChakraUI', 'Nivo'],
+    demoLink: 'https://habitsdashboard.netlify.app/#',
+    githubLink: 'https://github.com/Ahmed-O/HabitsDashboard',
+  },
+  {
+    id: 2,
     title: 'Pokemon Memory Game',
     image: 'images/pokemon.png',
     description: `Programmed a memory card game where the user is tasked with matching pairs of Pokemon cards before the time limit expires. Implemented three levels of difficulty alongside background music that the user can toggle on or off.`,
@@ -9,16 +18,16 @@ export const projectData = [
     githubLink: 'https://github.com/Ahmed-O/pokemon-memory-game',
   },
   {
-    id: 2,
+    id: 3,
     title: 'BluTube',
-    image: 'images/blutube.png',
+    image: '/images/blutube.png',
     description: `Implemented the UI for a Youtube clone using custom made React components. Allowed for search functionality using Youtube’s v3 data API.`,
     techStack: ['JSX', 'CSS', 'React', 'YouTube APIv3'],
     demoLink: 'https://youtube.com',
     githubLink: 'https://github.com/Ahmed-O/blutube',
   },
   {
-    id: 3,
+    id: 4,
     title: 'PixPaint',
     image: 'images/pixpaint.png',
     description: `Developed a website that allows users to create pixel sketches or drawings on a grid canvas. Included a color picker, eraser, clear button and an option to change the size of the drawing grid.`,
